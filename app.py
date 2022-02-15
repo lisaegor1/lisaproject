0b46c84ce5aae5849ffa499b50efa295fb54e54d
Yehor Lisachenko <83281922+lisaegor1@users.noreply.github.com>
2022-02-15 04:24:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,3 @@ app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-#app = Flask(__name__)
-
-#UPLOAD_DIRECTORY = "/home/yehor/TZ/uploads"
-
-#@app.route('/sg')
-#def index():
-#    return render_template("sg.html")
-
-
-#if __name__ == "__main__":
-#    app.run(debug=True)
