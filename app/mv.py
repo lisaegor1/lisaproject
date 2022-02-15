473de46901ae3f4500431fc97722e7543ecd8598
Yehor Lisachenko <83281922+lisaegor1@users.noreply.github.com>
2022-02-15 03:13:04 +0200

    FINAL VERSION
    update for script mv.py 
    new image for service1 in docker-compose

--- app/mv.py
+++ app/mv.py
@@ -1,11 +1,24 @@
-# Import path module from os
-
-from os import path
+# importing os module
+import os
 import shutil
 
 def mv():
 
-    source_path = "sg.txt"
-    path.exists(source_path)
-    destination_path = "templates"
-    shutil.move(source_path, destination_path)
+	path = '/app'
+	source = 'sg.txt'
+	destination = 'templates'
+	sourcePath = path+'/'+source
+	destinationPath = path+'/'+destination
+	if os.path.isdir(destinationPath+'/'+source):
+		print(source, 'exists in the destination path!')
+		shutil.rmtree(destinationPath+'/'+source)
+	
+	elif os.path.isfile(destinationPath+'/'+source):
+		os.remove(destinationPath+'/'+source)
+		print(source, 'deleted in', destination)
+
+
+	dest = shutil.move(sourcePath, destinationPath)
+
+
+
